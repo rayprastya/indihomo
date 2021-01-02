969ebf72c3f8bc66ed723cf11cbf5d249a0c88d0
rayprastya <rayhan.prastya@gmail.com>
2021-01-02 21:01:50 +0700

    update

---
+++ Vesperr/index.php
@@ -0,0 +1,35 @@
+<?php 
+  session_start();
+  include "db/db.php";
+
+  $url = $_SERVER['REQUEST_URI'];
+  $get_api = explode('/', $url);
+
+  if ($get_api[1] == 'api') {
+    include $get_api[1] . '/' . $get_api[2] . '.php';
+  } else {
+    if (!empty($_SESSION['role'])) {
+      include "head.php";
+      include "top-header.php";
+      include "sidebar.php";
+      include "panel.php";
+
+      if (empty($_GET['m']) and empty($_GET['s'])) {
+          include "pages/home/utama.php";
+      } else if ($_GET['m'] != "" and $_GET['s'] != "") {
+        if (file_exists("pages/".$_GET['m']."/".$_GET['s'].".php")) {
+          include "pages/".$_GET['m']."/".$_GET['s'].".php";
+        } else {
+          include "notfound.php";
+        }
+      }
+
+      include "footer.php";
+    } else {
+      include "login.php";
+    }
+  }
+
+
+?>
+
